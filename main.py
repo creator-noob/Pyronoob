a95d0e3566ad1b3add5b63c0d38a2c2c321cc09a
Noob Boy <108109585+creator-noob@users.noreply.github.com>
2022-07-02 07:58:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,6 +71,147 @@ async def restart_handler(_, m):
     await m.reply_text("Restarted!", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
+
+@bot.on_message(filters.command(["link"])& ~filters.edited)
+async def upload(bot: Client, m: Message):
+    editable = await m.reply_text('Send link in **Name&link** format to download')
+    input9: Message = await bot.listen(editable.chat.id)
+    raw = input9.text    
+    name = raw.split('&')[0]
+    url = raw.split('&')[1] or raw
+    await m.reply_text("**Enter resolution**")
+    input2: Message = await bot.listen(editable.chat.id)
+    raw_text2 = input2.text
+    
+    Show = f"**Downloading:-**\n\n**Name :-** ```{name}\nQuality - {raw_text2}```\n\n**Url :-** ```{url}```"
+    prog = await m.reply_text(Show)
+    
+    cc = f'>> **Name :** {name}'
+    
+    path = f"./downloads/"
+
+    if "youtu" in url:
+        if raw_text2 in ["144", "240", "480"]:
+            ytf = f'bestvideo[height<={raw_text2}][ext=mp4]+bestaudio[ext=m4a]'
+        elif raw_text2 == "360":
+            ytf = "18/134"
+        elif raw_text2 == "720":
+            ytf = "22/136/18"
+        elif raw_text2 =="1080":
+            ytf = "137/399"
+        else:
+            ytf = 18
+    else:
+        ytf=f"bestvideo[height<={raw_text2}]"
+
+    if "jwplayer" in url:
+        if raw_text2 in ["180", "144"]:
+            try:
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                ytf = f"{out['320x180 ']}/{out['256x144 ']}"
+            except Exception as e:
+                if e==0:
+                    raw_text2=="no"
+        elif raw_text2 in ["240", "270"]:
+            try:
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                ytf = f"{out['480x270 ']}/{out['426x240 ']}"
+            except Exception as e:
+                if e==0:
+                    raw_text2=="no"
+        elif raw_text2 == "360":
+            try:
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                ytf = out['640x360 ']
+            except Exception as e:
+                if e == 0:
+                    raw_text2=="no"
+                #cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+        elif raw_text2 == "480":
+            try:
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                ytf = f"{out['960x540 ']}/{out['852x480 ']}"
+            except Exception as e:
+                if e==0:
+                    raw_text2=="no"
+            # cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+        elif raw_text2 == "720":
+            try:
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                ytf = f"{out['1280x720 ']}"
+            except Exception as e:
+                if e==0:
+                    raw_text2=="no"
+            # cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+        elif raw_text2 == "1080":
+            try:
+                cmd = f'yt-dlp -F "{url}"'
+                k = await helper.run(cmd)
+                out = helper.vid_info(str(k))
+                ytf =f"{out['1920x1080 ']}/{['1920x1056']}"
+            except Exception as e:
+                if e==0:
+                    raw_text2=="no"
+        else:
+            # cmd = f'yt-dlp -F "{url}"'
+            # k = await helper.run(cmd)
+            #out = helper.vid_info(str(k))
+            # ytf = out['640x360 ']
+            #cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+            raw_text2=="no"
+#             except Exception as e:
+#                 print(e)
+
+    if ytf == f'bestvideo[height<={raw_text2}][ext=mp4]+bestaudio[ext=m4a]':
+        cmd = f'yt-dlp -o "{name}.mp4" -f "{ytf}" "{url}"'
+
+    # elif "jwplayer" in url:# and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+    #     cmd=f'yt-dlp -o "{name}.mp4" "{url}"'    
+    elif "adda247" in url:# and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+        cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+    elif "kdcampus" or "streamlock" in url:
+        cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+    elif ".pdf" in url: #and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+        cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
+    elif "drive" in url:
+        cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
+    elif raw_text2 == "no":# and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+        cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+#             elif "unknown" in ytf:
+#                 cmd=f'yt-dlp -o "{name}.mp4" "{url}"'
+    else:
+        cmd = f'yt-dlp -o "{name}.mp4" -f "{ytf}+bestaudio" "{url}"'
+
+    try:
+        download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
+        os.system(download_cmd)
+        filename = f"{name}.mp4"
+        subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
+        thumbnail = f"{filename}.jpg"
+        dur = int(helper.duration(filename))
+        await prog.delete (True)
+        try:
+            await m.reply_video(f"{name}.mp4",caption=cc, supports_streaming=True,height=720,width=1280,thumb=thumbnail,duration=dur)
+
+        except:
+            await m.reply_text("There was an error while uploading file as streamable so, now trying to upload as document.")
+            await m.reply_document(f"{name}.webm",caption=cc)
+        os.remove(f"{name}.mp4")
+        os.remove(f"{filename}.jpg")
+    except Exception as e:
+        await m.reply_text(e)
+
+
 @bot.on_message(filters.command(["pyro"])& ~filters.edited)
 async def account_login(bot: Client, m: Message):
     editable = await m.reply_text("Send txt file**")
